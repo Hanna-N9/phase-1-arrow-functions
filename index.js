b0c4618d557ef85c1cbf845f27d27305707bf28a
Hanna Negash <hnegash.n@gmail.com>
2023-09-11 20:19:06 -0700

    Write down function declaration and arrow function

---
+++ index.js
@@ -0,0 +1,12 @@
+//Function declaration
+function divide(a, b) {
+  return a / b;
+}
+
+//Arrow function with a parameter and multiplies it times itself
+const square = x => x * x;
+
+//Arrow function with two parameters, adds them together
+const add = (a, b) => {
+  return a + b;
+};
